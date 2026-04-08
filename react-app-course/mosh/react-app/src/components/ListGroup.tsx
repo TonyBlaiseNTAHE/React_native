c2b9c49a67b50465920b4d5cf3ee0e5c581d1f4f
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2026-04-08 11:02:43 +0200

    updates

--- react-app-course/mosh/react-app/src/components/ListGroup.tsx
+++ react-app-course/mosh/react-app/src/components/ListGroup.tsx
@@ -1,6 +1,7 @@
 function ListGroup() {
   return (
-    <div>
+    <>
+      <h1>List</h1>
       <ul className="list-group">
         <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
@@ -8,7 +9,7 @@ function ListGroup() {
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li>
       </ul>
-    </div>
+    </>
   );
 }
 export default ListGroup;

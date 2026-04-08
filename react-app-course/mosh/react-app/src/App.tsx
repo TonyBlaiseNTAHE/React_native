ef548c3f115b2c2681890d9989e821e8aa56f3f8
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2026-04-08 10:50:06 +0200

    creating a group list

--- react-app-course/mosh/react-app/src/App.tsx
+++ react-app-course/mosh/react-app/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 const App = () => {
   return (
     <div>
-      <Message />
+      <ListGroup />
     </div>
   );
 };
